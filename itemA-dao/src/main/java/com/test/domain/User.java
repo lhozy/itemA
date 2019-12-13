2984f94b601550a10e7ede39c256701472eb91cc
Hua <lhcn@qq.com>
2019-12-13 11:49:58 +0800

    张三分支修改完毕

--- itemA-dao/src/main/java/com/test/domain/User.java
+++ itemA-dao/src/main/java/com/test/domain/User.java
@@ -4,4 +4,28 @@ public class User {
     private String name;
     private String addr;
     private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
